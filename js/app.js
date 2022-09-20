699163ccd89d63e55381c576f410ed6e75290897
Casey Glidewell <caseyglidewell@gmail.com>
2022-09-20 14:57:19 -0700

    Refactor! turned my object literals into a singular constructor that makes all of em for me, and commented out object literals pending deletion.

--- js/app.js
+++ js/app.js
@@ -1,24 +1,152 @@
 'use strict';
 
 
-
-
-
 //GLOBALS
 
 let salesSection = document.getElementById('sales-data');
 
 console.dir(salesSection);
 
-//let hoursOfOperation = 15;
-
-//let hourlyCustomers = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,];
-
-//let hourlySales = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,];
-
 let listableSales = ['6AM: ', '7AM: ', '8AM: ', '9AM: ', '10AM: ', '11AM: ', '12PM: ', '1PM: ', '1PM: ', '2PM: ', '3PM: ', '4PM: ', '5PM: ', '6PM: ', '7PM: '];
 
-//let dailyTotal = 0;
+let storefronts = [];
+
+// CONSTRUCTOR
+
+function StoreLocation(city, minCust, maxCust, avgCookiesPerSale) {
+  this.city = city;
+  this.hoursOfOperation = 15;
+  this.minCust = minCust;
+  this.maxCust = maxCust;
+  this.avgCookiesPerSale = avgCookiesPerSale;
+  this.dailyTotal = 0;
+  this.hourlyCustomers = [];
+  this.hourlySales = [];
+  this.listableSales = [];
+  storefronts.push(this);
+
+}
+
+// CONSTRUCT OBJECTS
+
+new StoreLocation('Seattle', 23, 65, 6.3);
+new StoreLocation('Tokyo', 3, 24, 1.2);
+new StoreLocation('Dubai', 11, 38, 3.7);
+new StoreLocation('Paris', 20, 38, 2.3);
+new StoreLocation('Lima', 2, 16, 4.6);
+console.log(storefronts);
+
+
+
+
+
+// function renderAll() {
+//   for (let i = 0; i < storefronts.length; i++) {
+
+
+//   }
+
+// }
+
+
+
+
+
+// PROTOTYPE METHODS
+
+
+StoreLocation.prototype.getCustomers = function () {
+  console.log('PING!');
+  for (let i = 0; i < this.hoursOfOperation; i++) {
+    this.hourlyCustomers[i] = randomInt(this.minCust, this.maxCust);
+  }
+};
+
+StoreLocation.prototype.getSales = function () {
+  for (let i = 0; i < this.hoursOfOperation; i++) {
+    this.hourlySales[i] = this.hourlyCustomers[i] * this.avgCookiesPerSale;
+    this.hourlySales[i] = this.hourlySales[i].toFixed(0);
+    this.dailyTotal += Number(this.hourlySales[i]);
+  }
+};
+
+StoreLocation.prototype.getListableSales = function () {
+  for (let i = 0; i < this.hoursOfOperation; i++) {
+    this.listableSales[i] = listableSales[i] + this.hourlySales[i] + ' cookies';
+  }
+  this.listableSales.push('Total: ' + this.dailyTotal + ' cookies');
+
+};
+
+StoreLocation.prototype.render = function () {
+  // **** DOM ****
+  let articleElem = document.createElement('article');
+  salesSection.appendChild(articleElem);
+
+  let h2Elem = document.createElement('h2');
+  h2Elem.textContent = this.City;
+  articleElem.appendChild(h2Elem);
+
+  let ulElem = document.createElement('ul');
+  articleElem.appendChild(ulElem);
+
+  for (let i = 0; i < this.hoursOfOperation + 1; i++) {
+    let liElem = document.createElement('li');
+    liElem.textContent = this.listableSales[i];
+    ulElem.appendChild(liElem);
+  }
+  let tableElem = document.createElement('table');
+  articleElem.appendChild(tableElem);
+
+  let row1 = document.createElement('tr');
+  tableElem.appendChild(row1);
+
+  let th1Elem = document.createElement('th');
+  th1Elem.textContent = 'Good with Cats';
+  row1.appendChild(th1Elem);
+
+  let th2Elem = document.createElement('th');
+  th2Elem.textContent = 'Good with Dogs';
+  row1.appendChild(th2Elem);
+
+  let th3Elem = document.createElement('th');
+  th3Elem.textContent = 'Good with People';
+  row1.appendChild(th3Elem);
+
+  let row2 = document.createElement('tr');
+  tableElem.appendChild(row2);
+
+  let td1Elem = document.createElement('td');
+  td1Elem.textContent = this.isGoodWithCats;
+  row2.appendChild(td1Elem);
+
+  let td2Elem = document.createElement('td');
+  td2Elem.textContent = this.isGoodWithDogs;
+  row2.appendChild(td2Elem);
+
+  let td3Elem = document.createElement('td');
+  td3Elem.textContent = this.isGoodWithPeople;
+  row2.appendChild(td3Elem);
+
+};
+
+StoreLocation.prototype.sayHi = function () {
+  console.log('Hi!');
+};
+
+
+
+
+
+//! EXECUTE PROTOTYPE METHODS
+
+storefronts[0].sayHi();
+storefronts[0].getCustomers();
+storefronts[0].getSales();
+storefronts[0].getListableSales();
+
+
+
 
 //HELPER FUNCTIONS/UTILITIES
 
@@ -30,9 +158,9 @@ function randomInt(min, max) {
 
 
 //OBJECT LITERALS
-
+/*
 let Seattle = {
-  City: 'Seattle',
+  city: 'Seattle',
   hoursOfOperation: 15,
   minCust: 23,
   maxCust: 65,
@@ -296,33 +424,33 @@ let Lima = {
     }
   }
 };
+*/
 
 
 
 
+// EXECUTABLE CODE
+// Seattle.getCustomers();
+// Seattle.getSales();
+// Seattle.getListableSales();
+// Seattle.render();
 
-//EXECUTABLE CODE
-Seattle.getCustomers();
-Seattle.getSales();
-Seattle.getListableSales();
-Seattle.render();
+// Tokyo.getCustomers();
+// Tokyo.getSales();
+// Tokyo.getListableSales();
+// Tokyo.render();
 
-Tokyo.getCustomers();
-Tokyo.getSales();
-Tokyo.getListableSales();
-Tokyo.render();
+// Dubai.getCustomers();
+// Dubai.getSales();
+// Dubai.getListableSales();
+// Dubai.render();
 
-Dubai.getCustomers();
-Dubai.getSales();
-Dubai.getListableSales();
-Dubai.render();
+// Paris.getCustomers();
+// Paris.getSales();
+// Paris.getListableSales();
+// Paris.render();
 
-Paris.getCustomers();
-Paris.getSales();
-Paris.getListableSales();
-Paris.render();
-
-Lima.getCustomers();
-Lima.getSales();
-Lima.getListableSales();
-Lima.render();
+// Lima.getCustomers();
+// Lima.getSales();
+// Lima.getListableSales();
+// Lima.render();
